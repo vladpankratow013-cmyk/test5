30d0699cbb7f3b873ebe8763928515ce4482ab0b
vladpankratow013-cmyk <vladpankratow013@gmail.com>
2025-11-20 20:09:28 +0300

    created mainloop

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello worlf!')
+from pygame import *
+
+window = display.set_mode((900, 800))
+display.set_caption('Ping-pong')
+
+game = True
+timer = time.Clock()
+
+while game:
+    window.fill((200, 220, 255))
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    timer.tick(60)
